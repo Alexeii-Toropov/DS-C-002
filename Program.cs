791211b424e75ada5ca3e970b58921455882a46d
Alexeii Toropov <leha.toropov.04@mail.ru>
2022-08-11 17:59:26 +0300

    finish

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число обозначающее номер дня недели  ");
+int num = Convert.ToInt32(Console.ReadLine());
+    if(num == 6 || num == 7)
+    {
+        Console.WriteLine("да");
+    }
+    else if(num > 7 || num < 1)
+    {
+        Console.WriteLine("не является днем недели");
+    }
+    else
+    {
+        Console.WriteLine("не выходной");
+    }
