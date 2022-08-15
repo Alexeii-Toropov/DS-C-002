96309819446c12f69c29654641976e404316dbd2
Алексей <110294020+Alexeii-Toropov@users.noreply.github.com>
2022-08-15 15:18:09 +0300

    Add files via upload

---
+++ C#002.2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Ведите число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 && number > -100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    if (number < 0)
+    {
+        number = -1 * number;
+    }
+    string stnumber = Convert.ToString(number);
+    Console.WriteLine(stnumber[2]);
+}
